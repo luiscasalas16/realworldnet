ebd6621c0fc96a5300579df6f10bb9642c823849
Julian Solis <juliansolispa2002@gmail.com>
2024-05-03 16:59:04 -0600

    Proyecto de frontend.

---
+++ frontend-net/frontend-net/frontend-net/Controllers/AuthenticacionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace frontend_net.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
